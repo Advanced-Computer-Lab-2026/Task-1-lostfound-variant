403bb606de5b4ab86475236f383de5faabed8ef3
Dana <danaradwan139@gmail.com>
2026-09-23 17:19:09 +0300

    commit dana

--- server/src/models/Item.js
+++ server/src/models/Item.js
@@ -6,8 +6,11 @@ const itemSchema = new mongoose.Schema(
   {
     title: {type: String , required: true},
     description: {type: String },
-    category: {type:String , enum:[ 'electronics', 'clothing', 'documents', 'accessories',  ] , default: 'other' },
-    status: { type: String , enum: ['lost' , 'found' , 'claimed' ], default: 'lost'},
+category: { 
+  type: String, 
+  enum: ['electronics', 'clothing', 'documents', 'accessories', 'other'], 
+  default: 'other' 
+},    status: { type: String , enum: ['lost' , 'found' , 'claimed' ], default: 'lost'},
     location: {type: String },
 reportedBy: {
       type: mongoose.Schema.Types.ObjectId,
