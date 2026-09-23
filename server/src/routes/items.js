9c1df61c13396086de4339a75692b334e7e98cfa
alimmaherr <alimaherthebest@gmail.com>
2026-09-23 12:28:56 +0300

    commit 3

--- server/src/routes/items.js
+++ server/src/routes/items.js
@@ -7,8 +7,14 @@ import {
   deleteItem
 } from '../controllers/itemController.js';
 
+// TODO: wire up the routes described in README.md section 3.
+
 const router = Router();
 
-// TODO: wire up the routes described in README.md section 3.
+router.get('/', getAllItems);
+router.get('/:id', getItem);
+router.post('/', createItem);
+router.patch('/:id', updateItem);
+router.delete('/:id', deleteItem);
 
 export default router;
